d3263e3891edf69f550afc0b7fe822bf730b4f1d
PAragon <pablo@aragon.mx>
2020-06-06 01:15:37 -0500

    Primer commit, prepareando App

---
+++ pages/blog/_id.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h1>Detalle del artículo #{{$route.params.id}}</h1>
+    </div>
+</template>
---
+++ pages/blog/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-container>
+    <b-row>
+        <b-col md-3 v-for="(itm, ndx) in items" :key="ndx">
+            <h2>
+                <nuxt-link :to="`/blog/${itm.id}`">{{itm.description}}</nuxt-link>
+            </h2>
+        </b-col>
+    </b-row>
+  </b-container>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            items:[
+                {id:1, description:'Item #1'},
+                {id:2, description:'Item #2'},
+                {id:3, description:'Item #3'},
+                {id:4, description:'Item #4'}
+            ]
+        }
+    }
+}
+</script>
